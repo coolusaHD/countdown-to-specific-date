bc5fd4cdaaa40bdc37b3ef81b03a484b67fc0b1a
Philipp Alber <info@philippalber.de>
2024-02-19 17:42:44 +0100

    feat: init repo

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # countdown-to-specific-date
+
 GitHub Page for displaying a countdown to a specific date from an URL param
+
+## Usage
+
+To use this countdown, you can simply add the following URL parameters to the URL of this page:
+
+- `year`: The year of the date you want to count down to
+- `month`: The month of the date you want to count down to
+- `day`: The day of the date you want to count down to
+- `hour`: The hour of the date you want to count down to
+- `minute`: The minute of the date you want to count down to
+- `second`: The second of the date you want to count down to
+
+For example, if you want to count down to 2050-01-01 00:00:00, you can add the following URL parameters to the URL of this page:
+
+```
+?year=2050&month=1&day=1&hour=0&minute=0&second=0
+```
+
+## Customization
+
+You can customize the countdown by modifying the following query parameters in the URL:
+
+- `textColor`: The color of the countdown text (e.g., `?textColor=red` or `?textColor=#ff0000`)
+- `backgroundColor`: The background color of the countdown text (e.g., `?backgroundColor=black` or `?backgroundColor=#000000`)
+
+You can also customize the text after the digits by modifying the following query parameters in the URL:
+
+- `yearText`: The text after the year digits (e.g., `?yearText=years`)
+- `monthText`: The text after the month digits (e.g., `?monthText=months`)
+- `dayText`: The text after the day digits (e.g., `?dayText=days`)
+- `hourText`: The text after the hour digits (e.g., `?hourText=hours`)
+- `minuteText`: The text after the minute digits (e.g., `?minuteText=minutes`)
+- `secondText`: The text after the second digits (e.g., `?secondText=seconds`)
+
+## Examples
+
+- [Countdown to 2050-01-01 00:00:00](https://coolusahd.github.io/countdown-to-specific-date?year=2050&month=1&day=1&hour=0&minute=0&second=0&textColor=red&backgroundColor=black&yearText=years&monthText=months&dayText=days&hourText=hours&minuteText=minutes&secondText=seconds)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
+
+## Contributing
+
+Please read [CONTRIBUTING.md](CONTRIBUTING.md) for details on our code of conduct, and the process for submitting pull requests to us.
---
+++ assets/script.js
@@ -0,0 +1,111 @@
+// Set the countdown date
+
+// get date from url query
+var urlParams = new URLSearchParams(window.location.search);
+var countdownYear = urlParams.get('year');
+var countdownMonth = urlParams.get('month');
+var countdownDay = urlParams.get('day');
+var countdownHours = urlParams.get('hours');
+var countdownMinutes = urlParams.get('minutes');
+var countdownSeconds = urlParams.get('seconds');
+
+console.log(
+  'Date from url',
+  countdownYear,
+  countdownMonth,
+  countdownDay,
+  countdownHours,
+  countdownMinutes,
+  countdownSeconds
+);
+console.log(
+  'Date:',
+  new Date(
+    countdownYear,
+    countdownMonth - 1,
+    countdownDay,
+    countdownHours,
+    countdownMinutes,
+    countdownSeconds
+  ).toLocaleDateString()
+);
+
+// fallback to end of the year if no date is provided
+if (!countdownYear) {
+  countdownYear = new Date().getFullYear();
+}
+if (!countdownMonth) {
+  countdownMonth = 11;
+}
+if (!countdownDay) {
+  countdownDay = 31;
+}
+if (!countdownHours) {
+  countdownHours = 23;
+}
+if (!countdownMinutes) {
+  countdownMinutes = 59;
+}
+if (!countdownSeconds) {
+  countdownSeconds = 59;
+}
+
+var countDownDate = new Date(
+  countdownYear,
+  countdownMonth - 1,
+  countdownDay,
+  countdownHours,
+  countdownMinutes,
+  countdownSeconds
+).getTime();
+
+// set the text after the numbers from the url
+var daysText = urlParams.get('daysText') ?? 'Days';
+var hourText = urlParams.get('hourText') ?? 'Hours';
+var minuteText = urlParams.get('minuteText') ?? 'Minutes';
+var secondText = urlParams.get('secondText') ?? 'Seconds';
+
+document.getElementById('daysText').innerHTML = daysText;
+document.getElementById('hoursText').innerHTML = hourText;
+document.getElementById('minutesText').innerHTML = minuteText;
+document.getElementById('secondsText').innerHTML = secondText;
+
+// set the text color and background color from the url
+var textColor = urlParams.get('textColor');
+var backgroundColor = urlParams.get('backgroundColor');
+
+document.getElementById('bg').style.color = textColor;
+document.getElementById('bg').style.backgroundColor = backgroundColor;
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+  // Get the current time
+  var now = new Date().getTime();
+
+  // Find the distance between current time and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the corresponding elements
+  var daysString = days < 10 ? '0' + days : days;
+  var hoursString = hours < 10 ? '0' + hours : hours;
+  var minutesString = minutes < 10 ? '0' + minutes : minutes;
+  var secondsString = seconds < 10 ? '0' + seconds : seconds;
+  document.getElementById('days').innerHTML = daysString;
+  document.getElementById('hours').innerHTML = hoursString;
+  document.getElementById('minutes').innerHTML = minutesString;
+  document.getElementById('seconds').innerHTML = secondsString;
+  // If the count down is finished display Happy New Year text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById('days').innerHTML = '00';
+    document.getElementById('hours').innerHTML = '00';
+    document.getElementById('minutes').innerHTML = '00';
+    document.getElementById('seconds').innerHTML = '00';
+  }
+}, 1000);
